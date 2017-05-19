6d7c0c140e13f892499145b1b2edecf641edff46
Paul E. Long <paullo@microsoft.com>
2017-05-19 16:01:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Collection of PowerShell script to collect and analyze network and system logs and traces.
---
+++ SearchTrace/MASearch.ps1
@@ -0,0 +1,23 @@
+﻿Param(
+[parameter(Mandatory=$true, Position=1)]
+[alias("p")]
+$TracePath,
+[parameter(Mandatory=$true, Position=2)]
+[alias("f")]
+$Filter
+)
+
+Write-Host $TracePath
+Write-Host $Filter
+
+$d = Get-ChildItem -path $TracePath -include *.cap,*.etl,*.pcap,*.pcapng -Recurse
+$list = $d | Select-Object -Property FullName
+foreach($item in $list)
+{
+    Write-Host ($item.FullName)
+#  $s = New-PefTraceSession -Filter $Filter -Name $item.FullName -Silent
+#    Add-PefMessageProvider $item.FullName -PefSession $s
+#    Start-PefTraceSession -PefSession $s
+    New-PefTraceSession -Filter $Filter -Name $item.FullName -Silent | Add-PefMessageSource -Source $item.FullName | Start-PefTraceSession
+}
+
